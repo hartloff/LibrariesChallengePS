5763ee5d38f4a0cd92b21d25496bf5d5f9dfbfbc
jessehartloff <hartloff@buffalo.edu>
2017-04-04 15:15:31 -0400

    initial commit

---
+++ src/LibrariesChallengePS.java
@@ -0,0 +1,46 @@
+import cse115.math.Polynomial;
+import cse115.words.Words;
+
+public class LibrariesChallengePS{
+
+
+    // 5 points
+    static Polynomial Q1(Polynomial input, double x){
+        // return any Polynomial that intersects the input Polynomial at x
+
+        return null;
+    }
+
+
+    // 5 points
+    static boolean Q2(String possiblePhrase){
+        // return true if the input could be a phrase in the format "<adjective> <noun> <verb> <adverb>",
+        // false otherwise. Since many words have multiple parts of speech depending on their usage, return
+        // true if it is possible that the input can be in this format by any choice of each word's part of speech.
+        // You may assume the input contains 4 words separated by spaces.
+
+        return false;
+    }
+
+
+    // 5 points
+    static String Q3(String lyrics){
+        // Given a String with multiple lines, return its rhyme scheme with the first line labeled A and each new
+        // ending syllable is B, then C, etc. You don't have to identify if the scheme repeats so if the
+        // input has 10 lines that follow an AB rhyming scheme the output will be "ABABABABAB"
+        //
+        // In case this doesn't make sense, see Wikipedia's description of rhyme schemes which is more clear and
+        // and in depth than the above description: https://en.wikipedia.org/wiki/Rhyme_scheme
+        //
+        // You may assume that the input has at most 9 different rhymes (ie. will use the letters A-I)
+
+        return "";
+    }
+
+
+
+    public static void main(String[] args){
+
+    }
+
+}
